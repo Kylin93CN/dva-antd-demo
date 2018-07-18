8771e7a9cac9b11e32430e624f4d4946a4576539
Kylin93CN <375020612@qq.com>
2018-07-18 20:37:43 +0800

    使用history修改路由

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import dva from 'dva';
 import './index.css';
 import RouterConfig from './router'; // es6
+import { createBrowserHistory as createHistory } from 'history';
 
 // 1. Initialize
-const app = dva();
+const app = dva({
+  history: createHistory()
+});
 
 // 2. Plugins
 // app.use({});
