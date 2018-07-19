8a67d91bbe47f652ffd1a3174d207fc864f67308
Kylin93CN <375020612@qq.com>
2018-07-19 16:41:42 +0800

    添加dispatch

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { connect } from 'dva';
+import PropTypes from 'prop-types';
 
-const Counter = ({ counter }) => {
+const Counter = ({ counter, dispatch}) => {
   return (
     <div>
       <h1>
         {counter.count}
       </h1>
-      <button>+</button>
+      <button onClick={() => {dispatch({ type: 'counter/add' })}}>+</button>
     </div>
   )
+}
+
+Counter.propTypes = {
+  counter: PropTypes.object,
 }
 
 const mapStateToProps = (state) => {
