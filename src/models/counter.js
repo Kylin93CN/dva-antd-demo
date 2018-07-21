238190ffacc479b65e609ade8a5a68f41b7ae37f
Kylin93CN <375020612@qq.com>
2018-07-21 14:30:54 +0800

    reduces添加

--- src/models/counter.js
+++ src/models/counter.js
@@ -2,5 +2,17 @@ export default {
   namespace: 'counter',
   state: {
     count: 1,
-  }
+  },
+  reducers: {
+    add(state, action) { // 第一种感写法
+      return {
+        count: state.count + 1,
+      }
+    },
+    // 'add' (state, action) { // 第二种感写法，亦可写成‘aa/bb类’
+    //   return {
+    //     count: state.count + 1,
+    //   }
+    // },
+  },
 }
